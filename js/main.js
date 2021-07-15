7844a3a1d696ba004d0570e23c382b575804d4f2
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-15 16:20:12 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      // alert();
+      if(this.newItem == '') return;
+      
+      var todo = {
+        item: this.newItem
+      };
+
+      this.todos.push(todo);
+      this.newItem = '';
+    }
+  }
+})
